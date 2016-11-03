3d242bd361b9e4657d0c463549e71288a57b089e
Gretar Atli Gretarsson <gagre@kth.se>
2016-11-03 16:12:01 +0100

    added hangmanFXML

---
+++ HW1/src/main/java/se/kth/h16p02/npwj/hw1/client/HangmanFXMLClient.java
@@ -0,0 +1,18 @@
+package main.java.se.kth.h16p02.npwj.hw1.client;/**
+ * Created by GretarAtli on 03/11/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HangmanFXMLClient extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
