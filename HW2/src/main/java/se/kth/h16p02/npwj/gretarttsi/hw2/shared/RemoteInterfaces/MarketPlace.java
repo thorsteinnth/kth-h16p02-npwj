a052a6ed8fc278b611023f3e804d0b2cff6da395
Gretar Atli Gretarsson <gagre@kth.se>
2016-11-22 22:22:10 +0100

    MarketPlace and Trade remote interface added

---
+++ HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/shared/RemoteInterfaces/MarketPlace.java
@@ -0,0 +1,8 @@
+package se.kth.h16p02.npwj.gretarttsi.hw2.shared.RemoteInterfaces;
+
+import java.rmi.Remote;
+
+public interface MarketPlace extends Remote{
+
+
+}
---
+++ HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/shared/RemoteInterfaces/Trader.java
@@ -0,0 +1,6 @@
+package se.kth.h16p02.npwj.gretarttsi.hw2.shared.RemoteInterfaces;
+
+import java.rmi.Remote;
+
+public interface Trader extends Remote {
+}
