9b071a8d5c95bd144b3351644f197ca7699e0d76
Gretar Atli Gretarsson <gagre@kth.se>
2016-11-22 22:40:14 +0100

    functions added to marketplace and trader remote interfaces

--- HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/shared/RemoteInterfaces/Trader.java
+++ HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/shared/RemoteInterfaces/Trader.java
@@ -1,6 +1,12 @@
 package se.kth.h16p02.npwj.gretarttsi.hw2.shared.RemoteInterfaces;
 
 import java.rmi.Remote;
+import se.kth.h16p02.npwj.gretarttsi.hw2.shared.Domain.Item;
 
 public interface Trader extends Remote {
+
+    void wishIsAvailable (Item item);
+
+    void itemSold(Item item);
+
 }
