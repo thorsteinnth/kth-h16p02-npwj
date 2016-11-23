64d2d77c50a65425005ce41abcfdcdce9ca1f020
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-11-23 14:00:52 +0100

    marketplace interface work

--- HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/marketPlace/MarketPlaceImpl.java
+++ HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/marketPlace/MarketPlaceImpl.java
@@ -4,6 +4,7 @@ import se.kth.h16p02.npwj.gretarttsi.hw2.shared.Domain.Item;
 import se.kth.h16p02.npwj.gretarttsi.hw2.shared.RemoteInterfaces.MarketPlace;
 import se.kth.h16p02.npwj.gretarttsi.hw2.shared.RemoteInterfaces.Trader;
 
+import java.math.BigDecimal;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -15,21 +16,12 @@ public class MarketPlaceImpl extends UnicastRemoteObject implements MarketPlace
     }
 
     @Override
-    public void wish(Trader trader, Item item) throws RemoteException
+    public void addItemToWishlist(Trader trader, Item item, BigDecimal maxPrice) throws RemoteException
     {
-        System.out.println("Trader " + trader + " making a wish for item " + item);
-    }
-
-    @Override
-    public void register(Trader trader) throws RemoteException
-    {
-        System.out.println("Trader registering");
-    }
-
-    @Override
-    public void deregister(Trader trader) throws RemoteException
-    {
-        System.out.println("Trader deregistering");
+        System.out.println("Trader " + trader
+                + " adding item to wishlist with max price " + maxPrice
+                + ": "+ item
+        );
     }
 
     @Override
@@ -45,8 +37,31 @@ public class MarketPlaceImpl extends UnicastRemoteObject implements MarketPlace
     }
 
     @Override
-    public void inspect(Item item) throws RemoteException
+    public void inspectAvailableItems() throws RemoteException
     {
-        System.out.println("Should inspect item: " + item);
+        System.out.println("Should list available items");
     }
+
+    //region Registration handling
+
+    @Override
+    public void register(Trader trader) throws RemoteException
+    {
+        System.out.println("Trader registering");
+    }
+
+    @Override
+    public void deregister(Trader trader) throws RemoteException
+    {
+        System.out.println("Trader deregistering");
+    }
+
+    @Override
+    public boolean checkIfRegistered(Trader trader) throws RemoteException
+    {
+        System.out.println("Should check if trader is registered");
+        return false;
+    }
+
+    //endregion
 }
