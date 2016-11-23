a936c8d4c8c813078b4b873dae3b3070f30d055b
Gretar Atli Gretarsson <gagre@kth.se>
2016-11-23 11:01:58 +0100

    changes made to to traders

--- HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/shared/RemoteInterfaces/Trader.java
+++ HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/shared/RemoteInterfaces/Trader.java
@@ -1,12 +1,14 @@
 package se.kth.h16p02.npwj.gretarttsi.hw2.shared.RemoteInterfaces;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 import se.kth.h16p02.npwj.gretarttsi.hw2.shared.Domain.Item;
 
 public interface Trader extends Remote {
 
-    void wishIsAvailable (Item item);
+    void wishIsAvailable (String item) throws RemoteException;
 
-    void itemSold(Item item);
+    void itemSold(String item) throws RemoteException;
 
 }
