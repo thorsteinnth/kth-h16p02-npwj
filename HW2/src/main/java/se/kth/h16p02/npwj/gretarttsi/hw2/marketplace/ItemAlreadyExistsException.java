e13d607057a67432e09a00bc9e53a3d85e4daac0
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-11-24 21:13:12 +0100

    remove comments

--- HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/marketplace/ItemAlreadyExistsException.java
+++ HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/marketplace/ItemAlreadyExistsException.java
@@ -1,6 +1,7 @@
 package se.kth.h16p02.npwj.gretarttsi.hw2.marketplace;
 
-public class ItemAlreadyExistsException extends Exception {
+public class ItemAlreadyExistsException extends Exception
+{
     public ItemAlreadyExistsException(String message)
     {
         super(message);
--- HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/marketplace/TraderNotFoundException.java
+++ HW2/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw2/marketplace/TraderNotFoundException.java
@@ -1,8 +1,5 @@
 package se.kth.h16p02.npwj.gretarttsi.hw2.marketplace;
 
-/**
- * Created by GretarAtli on 23/11/2016.
- */
 public class TraderNotFoundException extends Exception
 {
     public TraderNotFoundException(String message) {
