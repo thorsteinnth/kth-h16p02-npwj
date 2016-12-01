94c6afff61701e70ed865172b7e6d3e41ee4f660
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-12-01 15:38:30 +0100

    username in saleitem and wishlistitem instead of trader

--- HW3/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw3/shared/domain/WishListItem.java
+++ HW3/src/main/java/se/kth/h16p02/npwj/gretarttsi/hw3/shared/domain/WishListItem.java
@@ -1,44 +1,33 @@
 package se.kth.h16p02.npwj.gretarttsi.hw3.shared.domain;
 
-import se.kth.h16p02.npwj.gretarttsi.hw3.shared.remoteInterfaces.Trader;
-
 import java.io.Serializable;
 import java.rmi.RemoteException;
 
 public class WishListItem implements Serializable
 {
-    private final Trader trader;
+    private String username;
     private final Item item;
 
     /**
      * WishListItem
-     * Trader is the trader that owns this wish list item
+     * Username is the name of the trader that owns this wish list item
      * Item is the item that he is wishing for. The price of the item is the max price that
      * the trader is willing to pay for the item.
      * */
-    public WishListItem(Trader trader, Item item)
+    public WishListItem(String username, Item item)
     {
-        this.trader = trader;
+        this.username = username;
         this.item = item;
     }
 
-    public Trader getTrader() {
-        return trader;
+    public String getUsername()
+    {
+        return username;
     }
 
-    public Item getItem() {
+    public Item getItem()
+    {
         return item;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        WishListItem that = (WishListItem) o;
-
-        if (!trader.equals(that.trader)) return false;
-        return item.equals(that.item);
     }
 
     public boolean equalsWithoutPrice(Object o) throws RemoteException
@@ -48,28 +37,42 @@ public class WishListItem implements Serializable
 
         WishListItem that = (WishListItem) o;
 
-        if (!trader.getUsername().equals(that.trader.getUsername())) return false;
+        if (!username.equals(that.username)) return false;
         return item.getName().equals(that.item.getName());
     }
 
     @Override
-    public int hashCode() {
-        int result = trader.hashCode();
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WishListItem that = (WishListItem) o;
+
+        if (!username.equals(that.username)) return false;
+        return item.equals(that.item);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = username.hashCode();
         result = 31 * result + item.hashCode();
         return result;
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "WishListItem{" +
-                "trader=" + trader +
+                "username='" + username + '\'' +
                 ", item=" + item +
                 '}';
     }
 
     public String toDisplayString() throws RemoteException{
         return "WishListItem{" +
-                "trader=" + trader.getUsername().toString() +
+                "username=" + username +
                 ", item=" + item.toDisplayString() +
                 '}';
     }
