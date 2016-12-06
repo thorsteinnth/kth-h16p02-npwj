099b99a1804433022ed96a3168aaf85d37fd7e61
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-12-06 15:24:28 +0100

    E04_2 initial commit

---
+++ E04_2/src/bank/model/OverdraftException.java
@@ -0,0 +1,13 @@
+package bank.model;
+
+/**
+ * Thrown when withdrawal would result in a negative balance.
+ */
+public class OverdraftException extends Exception {
+    private static final long serialVersionUID = 16247164402L;
+
+    public OverdraftException(String msg) {
+        super(msg);
+    }
+
+}
