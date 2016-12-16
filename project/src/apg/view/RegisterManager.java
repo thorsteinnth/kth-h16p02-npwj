86e4417a7b3c498f12ca07c41b6dd3c09ceff41e
gretarg09 <gretaratlig09@gmail.com>
2016-12-16 22:35:17 +0100

    adjustment on views

--- project/src/apg/view/RegisterManager.java
+++ project/src/apg/view/RegisterManager.java
@@ -4,12 +4,66 @@ import java.io.Serializable;
 import javax.ejb.EJB;
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 
-@Named("")
+@Named("registerManager")
+@RequestScoped
 public class RegisterManager
 {
+    private String email;
+    private String password;
+
+    private boolean showErrorEmail;
+    private boolean showErrorPassword;
+
+    //region ########## getters and setters ##########
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isShowErrorEmail() {
+        return showErrorEmail;
+    }
+
+    public void setShowErrorEmail(boolean showErrorEmail) {
+        this.showErrorEmail = showErrorEmail;
+    }
+
+    public boolean isShowErrorPassword() {
+        return showErrorPassword;
+    }
+
+    public void setShowErrorPassword(boolean showErrorPassword) {
+        this.showErrorPassword = showErrorPassword;
+    }
+
+//endregion
+
+    //region ########## Action Handlers ##########
+
+    public String add()
+    {
+        setShowErrorEmail(true);
+        setShowErrorPassword(true);
+        return "success";
+    }
+
+    //endregion
 
 }
