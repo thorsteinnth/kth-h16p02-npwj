3c345c8bb2479fa63c49eea0fbe56c3408f155a8
Þorsteinn Þorri Sigurðsson <thorsteinnth@gmail.com>
2016-12-17 15:05:05 +0100

    items table + bootstrap with some gnomes

--- project/src/resources/META-INF/bootstrap_data.sql
+++ project/src/resources/META-INF/bootstrap_data.sql
@@ -1,1 +1,6 @@
 INSERT INTO USERS (EMAIL, PASSW, ISADMIN, ISBANNED) VALUES ('thorsteinnth@gmail.com', 'mypassword', 1, 0)
+INSERT INTO USERS (EMAIL, PASSW, ISADMIN, ISBANNED) VALUES ('gretarg09@gmail.com', 'mypassword', 1, 0)
+
+INSERT INTO ITEM (SKU, DESCRIPTION, PRICE) VALUES ('argur', 'Argur is mad', 999)
+INSERT INTO ITEM (SKU, DESCRIPTION, PRICE) VALUES ('bjarmi', 'Bjarmi is bright', 999)
+INSERT INTO ITEM (SKU, DESCRIPTION, PRICE) VALUES ('bjarmi-bluehat-redpants', 'Bjarmi is bright. This one has a blue hat and red pants!', 1249)
