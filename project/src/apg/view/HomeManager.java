c63a11f2114bbb96dcc8878507081eca0e370e63
gretarg09 <gretaratlig09@gmail.com>
2016-12-17 15:10:11 +0100

    bla

---
+++ project/src/apg/view/HomeManager.java
@@ -0,0 +1,14 @@
+package apg.view;
+
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named("homeManager")
+@SessionScoped
+public class HomeManager implements Serializable{
+
+
+}
