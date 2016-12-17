8d23ad322160c9a4e7a10496ebc8f2c78c7cf100
gretarg09 <gretaratlig09@gmail.com>
2016-12-17 17:50:24 +0100

    added shoppingcardmanager and shoppingCard

---
+++ project/src/apg/view/ShoppingCardManager.java
@@ -0,0 +1,13 @@
+package apg.view;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named("homeManager")
+@SessionScoped
+public class ShoppingCardManager implements Serializable
+{
+    
+
+}
